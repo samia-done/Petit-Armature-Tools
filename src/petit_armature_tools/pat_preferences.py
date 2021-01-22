7c691f4d2c4ae7f418b5941f699e641b54bc48a4
Samia <necogane+bb@gmail.com>
2021-01-22 12:22:17 +0900

    Fix settings
    パラメータの読み込み先を、ユーザープリファレンスからシーンのツール設定に変更。

--- src/petit_armature_tools/pat_preferences.py
+++ src/petit_armature_tools/pat_preferences.py
@@ -58,62 +58,6 @@ def update_panel(self, context):
 class PAT_AddonPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
-    bone_name = bpy.props.StringProperty(
-        name="Bone Name",
-        description="Bone name",
-        default="Bone"
-    )
-
-    bone_name_junction = bpy.props.StringProperty(
-        name="Bone Name Separator",
-        description="Bone name separator",
-        default="."
-    )
-
-    bone_name_prefix = bpy.props.StringProperty(
-        name="Bone Name Prefix",
-        description="Bone name prefix",
-        default=""
-    )
-
-    bone_name_suffix = bpy.props.StringProperty(
-        name="Bone Name Suffix",
-        description="Bone name suffix",
-        default=""
-    )
-
-    start_number = bpy.props.IntProperty(
-        name="Start Number",
-        description="Starting number of bone name",
-        default=1,
-        min=0,
-    )
-
-    zero_padding = bpy.props.IntProperty(
-        name="Zero-padding",
-        description="Zero-padding of digits in bone names",
-        default=3,
-        min=1,
-    )
-
-    is_reverse = bpy.props.BoolProperty(
-        name="Reverse",
-        description="Change the bone order to the reverse order",
-        default=False
-    )
-
-    is_parent = bpy.props.BoolProperty(
-        name="Parent",
-        description="Set parent bone",
-        default=True
-    )
-
-    use_connect = bpy.props.BoolProperty(
-        name="Connected",
-        description="When Bone has a parent,bone's head is stuck tp the parent's tail",
-        default=True
-    )
-
     category = bpy.props.StringProperty(
         name="Tab Category",
         description="Choose a name for the category of the panel",
@@ -133,17 +77,6 @@ class PAT_AddonPreferences(bpy.types.AddonPreferences):
 
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Create Bone Settings")
-        layout.prop(self, "bone_name")
-        layout.prop(self, "bone_name_junction")
-        layout.prop(self, "bone_name_prefix")
-        layout.prop(self, "bone_name_suffix")
-        layout.prop(self, "start_number")
-        layout.prop(self, "zero_padding")
-        layout.prop(self, "is_reverse")
-        layout.prop(self, "is_parent")
-        layout.prop(self, "use_connect")
-
         row = layout.row()
         col = row.column()
         col.label(text="Tab Category:")
